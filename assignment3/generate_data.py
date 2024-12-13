1aaa61525e77b2f1c88c771bbe4b027d3c24c96e
AlaaElkhouly <161586676+AlaaElkhouly@users.noreply.github.com>
2024-12-13 17:06:20 +0200

    k

--- assignment3/generate_data.py
+++ assignment3/generate_data.py
@@ -7,8 +7,8 @@ id_2 = 7818 #change to second student id
 id_3 = 0000 #change to third student id "leave 0000 if team of 2"
 random_seed = id_1+id_2+id_3
 random.seed(random_seed)
-data_path="C:\Term 9\Artificail Intellegnce\REPO\machine_learning\assignment3\Data.csv"#replace with data path
-output_path="C:\Term 9\Artificail Intellegnce\REPO\machine_learning\assignment3\MyData.csv"#replace with output data path
+data_path=r"C:\Term 9\Artificail Intellegnce\REPO\machine_learning\assignment3\Data.csv"#replace with data path
+output_path=r"C:\Term 9\Artificail Intellegnce\REPO\machine_learning\assignment3\MyData.csv"#replace with output data path
 
 all_data=pd.read_csv(data_path) 
 all_columns = all_data.columns.tolist()
